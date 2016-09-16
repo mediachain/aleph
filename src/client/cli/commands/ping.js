203432ef693b110c91e1162b05eb0e66be0e9be1
Yusef Napora <yusef@napora.org>
2016-09-16 16:51:27 -0400

    use default port for ping

--- src/client/cli/commands/ping.js
+++ src/client/cli/commands/ping.js
@@ -7,7 +7,7 @@ const describe = 'ping a remote node';
 const handler = ({peerId}) => {
     console.log('pinging peer: ', peerId);
 
-    client = new RestClient({rootURL: 'http://localhost'});
+    client = new RestClient({rootURL: 'http://localhost:9002'});
     client.ping(peerId);
 };
 
