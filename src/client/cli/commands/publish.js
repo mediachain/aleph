8a46896ab3d584ef421179f5e7e9094749ae9dc8
Yusef Napora <yusef@napora.org>
2016-09-19 15:00:33 -0400

    just accept statement string from cli, not stringified json

--- src/client/cli/commands/publish.js
+++ src/client/cli/commands/publish.js
@@ -5,15 +5,13 @@ const RestClient = require('../../api/RestClient');
 module.exports = {
     command: 'publish <namespace> <statement>',
     description: 'publish a statement',
-    builder: (yargs: any): any => {
-        return yargs.coerce('statement', JSON.parse)
-    },
 
-    handler: (opts: {namespace: string, peerUrl: string, statement: Object}) => {
+    handler: (opts: {namespace: string, peerUrl: string, statement: string}) => {
         const {namespace, peerUrl, statement} = opts;
         const client = new RestClient({peerUrl});
 
-        client.publish(namespace, statement)
+
+        client.publish(namespace, {object: statement})
             .then(
                 response => { console.log(response); },
                 err => { console.error(err); }
