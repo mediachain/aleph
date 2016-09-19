309edbf4da120b58af81ad3e709e9a4aac4adb8e
Yusef Napora <yusef@napora.org>
2016-09-19 14:26:48 -0400

    add usage text, --help argument

--- src/client/cli/index.js
+++ src/client/cli/index.js
@@ -3,6 +3,10 @@
 const RestClient = require('../api/RestClient');
 
 const argv = require('yargs')
+    .usage('Usage: $0 [options] <command> [command-options]')
+    .help()
+    .example('$0 ping QmF00123', 'send a ping message to peer with id QmF00123')
+    .demand(1, 'Missing command argument')
     .option('apiUrl', {
         alias: 'a',
         description: 'root URL of the REST API for a mediachain node',
