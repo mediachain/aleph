4aa7b94f6d5194ab6bd8233fb9d329d26f8b6fd2
Yusef Napora <yusef@napora.org>
2016-09-26 11:19:22 -0400

    fix pre-push script bug when running against clean-repo

--- pre-push.sh
+++ pre-push.sh
@@ -9,15 +9,19 @@ if [ -e "$HOME/.nvm/nvm.sh" ]; then
 fi
 
 # stash un-staged changes
+NUM_STASHES_BEFORE=$(git stash list | wc -l)
 git stash -q --keep-index
+NUM_STASHES_AFTER=$(git stash list | wc -l)
 
 # run standard (code style enforcer), flow, and tests
 npm run check && npm run test
 
 RESULT=$?
 
-# un-stash
-git stash pop -q
+# un-stash, if the previous stash command actually created a stash
+if [ "$NUM_STASHES_AFTER" -gt "$NUM_STASHES_BEFORE" ]; then
+    git stash pop -q
+fi
 
 # abort commit if anything failed
 [ $RESULT -ne 0 ] && exit 1
