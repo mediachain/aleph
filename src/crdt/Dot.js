d089266e048382af44902aeff63d45984602416a
Yusef Napora <yusef@napora.org>
2016-09-28 14:41:07 -0400

    make default Dot id empty string instead of undefined

--- src/crdt/Dot.js
+++ src/crdt/Dot.js
@@ -6,7 +6,7 @@ import type { KeyType } from './types'
 // really a positive integer, but no way to encode that as a type
 export type DotClock = number
 
-class Dot extends Record({id: undefined, clock: 1}) {
+class Dot extends Record({id: '', clock: 1}) {
   constructor (id: KeyType, clock?: DotClock) {
     super({
       id,
