38fb9e3d3744e3e7520386385f7ce2d9a8a8ae1a
Yusef Napora <yusef@napora.org>
2016-09-28 11:10:23 -0400

    add Add-Wins ORSet type

---
+++ src/crdt/AWORSet.js
@@ -0,0 +1,77 @@
+// @flow
+
+const { Record, Set: ISet, is: isEqual } = require('immutable')
+const { DotContext } = require('./DotContext')
+const { DotKernel, DotKernelDelta } = require('./DotKernel')
+import type { CRDT, ReplicaID } from './types'
+
+/**
+ * An add-wins observed-remove set
+ */
+class AWORSet<V: CRDT> extends Record({
+  id: '',
+  kernel: new DotKernel()
+}) {
+  constructor (id: ReplicaID, contextOrKernel?: DotContext | DotKernel<V>) {
+    let kernel: DotKernel<V>
+    if (contextOrKernel == null) {
+      kernel = new DotKernel(new DotContext())
+    } else if (contextOrKernel instanceof DotContext) {
+      kernel = new DotKernel(contextOrKernel)
+    } else {
+      kernel = contextOrKernel
+    }
+
+    super({id, kernel})
+  }
+
+  get id(): ReplicaID { return this.id }
+  get kernel(): DotKernel<V> { return this.get('kernel') }
+  get context(): DotContext { return this.kernel.context }
+
+  read (): ISet<V> {
+    return new ISet(this.kernel.dots.values())
+  }
+
+  contains (val: V): boolean {
+    for (const v of this.kernel.dots.values()) {
+      if (isEqual(v, val)) {
+        return true
+      }
+    }
+    return false
+  }
+
+  add (val: V): AWORSet<V> {
+    return this.join(this.addDelta(val))
+  }
+
+  addDelta (val: V): AWORSetDelta<V> {
+    const kernelDelta = this.kernel.addDelta(this.id, val)
+    return new AWORSetDelta({kernel: kernelDelta})
+  }
+
+  remove (val: V): AWORSet<V> {
+    return this.join(this.removeDelta(val))
+  }
+
+  removeDelta (val: V): AWORSetDelta<V> {
+    const kernelDelta = this.kernel.removeValueDelta(val)
+    return new AWORSetDelta({kernel: kernelDelta})
+  }
+
+  join (other: AWORSet<V> | AWORSetDelta<V>): AWORSet<V> {
+    return new AWORSet(this.id, this.kernel.join(other.kernel))
+  }
+}
+
+class AWORSetDelta<V> extends Record({
+  kernel: new DotKernelDelta()
+}) {
+  get kernel(): DotKernelDelta<V> { return this.get('kernel') }
+}
+
+module.exports = {
+  AWORSet,
+  AWORSetDelta
+}
