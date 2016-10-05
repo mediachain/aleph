ce70a50dd402ea9f245ac54786d3363d6b94e80c
Yusef Napora <yusef@napora.org>
2016-10-05 14:58:43 -0400

    use fetch instead of cujojs rest package, which doesn't support streaming

--- src/client/api/RestClient.js
+++ src/client/api/RestClient.js
@@ -1,23 +1,37 @@
 // @flow
 
-const rest = require('rest')
-const errorCode = require('rest/interceptor/errorCode')
+const fetch: (url: string, opts?: Object) => Promise<FetchResponse> = require('node-fetch')
 
+import type { Transform as TransformStream } from 'stream'
 import type { StatementMsg, SimpleStatementMsg } from '../../protobuf/types'
-
 export type NodeStatus = 'online' | 'offline' | 'public'
 
-type RestResponse = { entity: string | Object }
+type FetchResponse = {
+  text: () => Promise<string>,
+  json: () => Promise<Object>,
+  buffer: () => Promise<Buffer>,
+  body: TransformStream,
+  statusText: string,
+  status: number,
+  ok: boolean
+}
+
+class RestError extends Error {
+  response: FetchResponse
+  statusCode: number
+  constructor (response) {
+    super(response.statusText)
+    this.statusCode = response.status
+    this.response = response
+  }
+}
 
 class RestClient {
   peerUrl: string;
   client: Function;
 
   constructor (options: {peerUrl?: string}) {
-    let {peerUrl} = options
-    this.peerUrl = peerUrl || ''
-    this.client = rest
-      .wrap(errorCode)
+    this.peerUrl = options.peerUrl || ''
   }
 
   _makeUrl (path: string): string {
@@ -25,34 +39,35 @@ class RestClient {
     return this.peerUrl + absPath
   }
 
-  req (path: string, args: Object = {}): Promise<RestResponse> {
-    args.path = this._makeUrl(path)
-    return this.client(args)
-      .catch(errorResponse => {
-        const err = errorResponse.error || new Error(errorResponse.entity || 'unknown error')
-        err.response = errorResponse
-        throw err
+  req (path: string, args: Object = {}): Promise<FetchResponse> {
+    const fullUrl = this._makeUrl(path)
+    return fetch(fullUrl, args)
+      .then(response => {
+        if (!response.ok) {
+          throw new RestError(response)
+        }
+        return response
       })
   }
 
-  getRequest (path: string): Promise<RestResponse> {
+  getRequest (path: string): Promise<FetchResponse> {
     return this.req(path)
   }
 
-  postRequest (path: string, body: Object | string, isJSON: boolean = true): Promise<RestResponse> {
+  postRequest (path: string, body: Object | string, isJSON: boolean = true): Promise<FetchResponse> {
     return this.req(path, {
       method: 'POST',
       headers: isJSON ? { 'Content-Type': 'application/json' } : {},
-      entity: isJSON ? JSON.stringify(body) : body
+      body: isJSON ? JSON.stringify(body) : body
     })
   }
 
   id (): Promise<string> {
     return this.getRequest('id')
-      .then(r => r.entity)
+      .then(r => r.text())
   }
 
-  ping (peerId: string): Promise<bool> {
+  ping (peerId: string): Promise<boolean> {
     return this.getRequest(`ping/${peerId}`)
       .then(response => true)
   }
@@ -60,36 +75,34 @@ class RestClient {
   publish (namespace: string, statement: SimpleStatementMsg): Promise<string> {
     console.log(`publishing ${JSON.stringify(statement)} to ${namespace}`)
     return this.postRequest(`publish/${namespace}`, statement)
-      .then(r => r.entity)
+      .then(r => r.text())
   }
 
   statement (statementId: string): Promise<StatementMsg> {
     return this.getRequest(`stmt/${statementId}`)
-      .then(r => r.entity)
-      .then(JSON.parse)
+      .then(r => r.json())
   }
 
   query (queryString: string): Promise<Object> {
     return this.postRequest('query', queryString, false)
-      .then(r => r.entity)
-      .then(JSON.parse)
+      .then(r => r.json())
   }
 
   getStatus (): Promise<NodeStatus> {
     return this.getRequest('status')
-      .then(r => r.entity)
+      .then(r => r.text())
       .then(validateStatus)
   }
 
   setStatus (status: NodeStatus): Promise<NodeStatus> {
     return this.postRequest(`status/${status}`, '', false)
-      .then(r => r.entity)
+      .then(r => r.text())
       .then(validateStatus)
   }
 
   getDirectoryId (): Promise<string> {
     return this.getRequest('config/dir')
-      .then(r => r.entity)
+      .then(r => r.text())
   }
 
   setDirectoryId (id: string): Promise<boolean> {
