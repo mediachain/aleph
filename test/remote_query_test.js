48a778b8a89c73aaf99ac35db695e84e1b7c0154
Yusef Napora <yusef@napora.org>
2016-10-07 11:50:16 -0400

    add test for remote query error handling

--- test/remote_query_test.js
+++ test/remote_query_test.js
@@ -26,9 +26,18 @@ const queryHandler = (results: Array<QueryResultMsg>) => (conn: Connection) => p
   conn
 )
 
+function mockRemote (results: Array<QueryResultMsg>): Node {
+  const node = new Node(generateIdentity())
+  node.p2p.handle(PROTOCOLS.node.query, queryHandler(results))
+  return node
+}
+
+function startNodes (...nodes: Array<Node>): Promise<*> {
+  return Promise.all(nodes.map(n => n.start()))
+}
+
 describe('Remote Query', () => {
   const local = new Node(generateIdentity())
-  const remote = new Node(generateIdentity())
 
   it('decodes all query result types correctly', function () {
     this.timeout(3000)
@@ -44,17 +53,16 @@ describe('Remote Query', () => {
       {end: {}}
     ]
 
+    // the stream doesn't deliver the "end" response, it just ends the stream
     const expected = responses.slice(0, responses.length - 1)
+    const remote = mockRemote(responses)
 
-    remote.p2p.handle(PROTOCOLS.node.query, queryHandler(responses))
-
-    return Promise.all([local.start(), remote.start()])  // start both peers
+    return startNodes(local, remote) // start both peers
       .then(() => local.remoteQuery(remote.p2p.peerInfo, 'SELECT * FROM foo.bar'))
       .then(resultStream =>
         new Promise(resolve => {
           pull(
             resultStream,
-            pull.through(val => console.dir(val, {depth: 100})),
             pull.collect((err, results) => {
               assert(err == null, 'query should not return an error')
               assert.deepEqual(results, expected, 'query should return all expected results')
@@ -62,5 +70,33 @@ describe('Remote Query', () => {
             }))
         })
       )
+  })
+
+  it('ends the stream with an error when it gets an error response', function () {
+    this.timeout(3000)
+
+    const errorMessage = 'server on fire'
+    const responses = [
+      {value: {simple: {stringValue: 'hello world'}}},
+      {value: {simple: {intValue: 123}}},
+      {error: {error: errorMessage}}
+    ]
+
+    const remote = mockRemote(responses)
+    const expected = responses.slice(0, responses.length - 1)
+
+    return startNodes(local, remote)
+      .then(() => local.remoteQuery(remote.p2p.peerInfo, 'SELECT * FROM foo.bar'))
+      .then(resultStream => new Promise(resolve => {
+        pull(
+          resultStream,
+          pull.collect((err, results) => {
+            assert(err instanceof Error, 'query should return an error object')
+            assert.equal(err.message, errorMessage, 'error object should have error message from remote node')
+            assert.deepEqual(results, expected, 'we should still get all results before the error')
+            resolve()
+          })
+        )
+      }))
   })
 })
