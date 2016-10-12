5c0ada817c07c7e5506593ce443cf72d9a172cb2
Yusef Napora <yusef@napora.org>
2016-10-12 13:19:16 -0400

    add dockerfile and build script for concat-dependencies image

---
+++ integration-test/concat-dependencies/build-and-push.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# helper script to build the concat-dependencies image and push to docker hub
+# you must be logged in to docker hub (with 'docker login') first, or the push will fail.
+#
+# accepts a tag for the image as the first argument, or will default to 'latest'
+# if no tag is given
+
+# set current directory to this dir, in case we're running from elsewhere
+repo_root=$(git rev-parse --show-toplevel)
+dir="${repo_root}/integration-test/concat-dependencies"
+
+cd "${dir}"
+
+image_tag=$1
+if [ "$image_tag" == "" ]; then
+    image_tag="latest"
+fi
+
+full_tag="mediachain/concat-dependencies:${image_tag}"
+
+docker build -t ${full_tag} .
+status=$?
+if [ ${status} -ne 0 ]; then
+    echo "Error building image, not uploading to docker hub"
+    exit ${status}
+fi
+
+echo "image \"${full_tag}\" built successfully, pushing to docker hub"
+docker push ${full_tag}
+
+status=$?
+if [ ${status} -ne 0 ]; then
+    echo "Pushing to docker hub failed.  You may need to 'docker login' first."
+fi
+exit ${status}
