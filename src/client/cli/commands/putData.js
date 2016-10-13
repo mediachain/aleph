56948405c1950586145a494b509bdf67d5f1c3c4
Yusef Napora <yusef@napora.org>
2016-10-13 17:55:54 -0400

    add batching to putData cli command, default batch size = 1000

--- src/client/cli/commands/putData.js
+++ src/client/cli/commands/putData.js
@@ -3,26 +3,34 @@
 const ndjson = require('ndjson')
 const RestClient = require('../../api/RestClient')
 
+const BATCH_SIZE = 1000
+
 module.exports = {
   command: 'putData',
   description: 'read newline-delimited JSON data from standard input and store in the remote node as IPLD',
+  builder: {
+    batchSize: { default: BATCH_SIZE }
+  },
 
-  handler: (opts: {peerUrl: string}) => {
-    const {peerUrl} = opts
+  handler: (opts: {peerUrl: string, batchSize: number}) => {
+    const {peerUrl, batchSize} = opts
     const client = new RestClient({peerUrl})
 
-    const items: Array<Object> = []
+    let items: Array<Object> = []
+
     process.stdin.pipe(ndjson.parse())
-      .on('data', obj => items.push(obj))
-      .on('error', err => console.error('Error reading from stdin: ', err))
-      .on('end', () => {
-        client.putData(...items)
-          .then(
+      .on('data', obj => {
+        items.push(obj)
+        if (items.length >= batchSize) {
+          client.putData(...items).then(
             hashes => {
               hashes.forEach(h => console.log(h))
             },
             err => console.error(err.message)
           )
+          items = []
+        }
       })
+      .on('error', err => console.error('Error reading from stdin: ', err))
   }
 }
