85ed5fdbf028d86752018b5a78096b2a5f94df01
Yusef Napora <yusef@napora.org>
2016-10-13 18:12:34 -0400

    accept optional filename for putData

--- src/client/cli/commands/putData.js
+++ src/client/cli/commands/putData.js
@@ -1,24 +1,34 @@
 // @flow
 
+const fs = require('fs')
 const ndjson = require('ndjson')
 const RestClient = require('../../api/RestClient')
+import type { Readable } from 'stream'
 
 const BATCH_SIZE = 1000
 
+
 module.exports = {
-  command: 'putData',
-  description: 'read newline-delimited JSON data from standard input and store in the remote node as IPLD',
+  command: 'putData [filename]',
+  description: 'read newline-delimited JSON data from `filename` or stdin and store in the remote node as IPLD',
   builder: {
     batchSize: { default: BATCH_SIZE }
   },
 
-  handler: (opts: {peerUrl: string, batchSize: number}) => {
-    const {peerUrl, batchSize} = opts
+  handler: (opts: {peerUrl: string, batchSize: number, filename: ?string}) => {
+    const {peerUrl, batchSize, filename} = opts
     const client = new RestClient({peerUrl})
 
     let items: Array<Object> = []
 
-    process.stdin.pipe(ndjson.parse())
+    let inputStream: Readable
+    if (filename) {
+      inputStream = fs.createReadStream(filename)
+    } else {
+      inputStream = process.stdin
+    }
+
+    inputStream.pipe(ndjson.parse())
       .on('data', obj => {
         items.push(obj)
         if (items.length >= batchSize) {
