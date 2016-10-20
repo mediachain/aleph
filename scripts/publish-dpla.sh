a3cde3647b76a7789413174c4688d3632318100d
vyzo <vyzo@hackzen.org>
2016-10-20 13:18:28 +0300

    scripts/publish-dpla.sh: fix regexp expansion (was ending up quoted), drop the dry-run

--- scripts/publish-dpla.sh
+++ scripts/publish-dpla.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 NAMESPACE="images.dpla"
+REGEX="(dpla_)http.*/(.*)"
 CONTENT_SELECTOR="--contentSelector _source"
 ID_SELECTOR="--idSelector native_id"
-ID_REGEXP="--idRegex '(dpla_)http.*/(.*)'"
+ID_REGEXP="--idRegex $REGEX"
 CONTENT_FILTERS="--contentFilters aesthetics"
 
-mcclient publish --dryRun ${CONTENT_SELECTOR} ${ID_SELECTOR} ${ID_REGEXP} ${CONTENT_FILTERS} ${NAMESPACE} $1 > /dev/null
+mcclient publish ${CONTENT_SELECTOR} ${ID_SELECTOR} ${ID_REGEXP} ${CONTENT_FILTERS} ${NAMESPACE} $1 > /dev/null
 
