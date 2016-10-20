cdb6e13f3d9d0f1d7113f656ed7ea70370532de6
vyzo <vyzo@hackzen.org>
2016-10-20 18:04:36 +0300

    scripts/publish-500px

--- scripts/publish-dpla.sh
+++ scripts/publish-dpla.sh
@@ -3,9 +3,9 @@
 NAMESPACE="images.dpla"
 REGEX="(dpla_)http.*/(.*)"
 CONTENT_SELECTOR="--contentSelector _source"
+CONTENT_FILTERS="--contentFilters aesthetics"
 ID_SELECTOR="--idSelector native_id"
 ID_REGEXP="--idRegex $REGEX"
-CONTENT_FILTERS="--contentFilters aesthetics"
 
 mcclient publish ${CONTENT_SELECTOR} ${ID_SELECTOR} ${ID_REGEXP} ${CONTENT_FILTERS} ${NAMESPACE} $1 > /dev/null
 
