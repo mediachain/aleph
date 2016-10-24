90da368191d4c172cb88ed6eb735f863c4d787f5
Yusef Napora <yusef@napora.org>
2016-10-24 11:16:11 -0400

    add --info flag to listPeers command to fetch info strings

--- src/client/cli/commands/listPeers.js
+++ src/client/cli/commands/listPeers.js
@@ -4,13 +4,49 @@ const RestClient = require('../../api/RestClient')
 
 module.exports = {
   command: 'listPeers',
+  builder: {
+    info: {
+      type: 'boolean',
+      alias: 'i',
+      default: false,
+      description: 'Also fetch the "info" string for each peer.  This requires an extra network request per-peer.\n'
+    }
+  },
   description: `fetch a list of peers from the node's directory server.\n`,
-  handler: (opts: {apiUrl: string}) => {
-    const {apiUrl} = opts
+  handler: (opts: {apiUrl: string, info: boolean}) => {
+    const {apiUrl, info} = opts
     const client = new RestClient({apiUrl})
     client.listPeers().then(
-      peers => { peers.forEach(p => console.log(p)) },
+      peers => {
+        if (info) {
+          fetchInfos(client, peers)
+        } else {
+          peers.forEach(p => console.log(p))
+        }
+      },
       err => { console.error(err.message) }
     )
   }
+}
+
+function fetchInfos (client: RestClient, peerIds: Array<string>) {
+  let promises = []
+  for (const peer of peerIds) {
+    promises.push(
+      client.id(peer)
+        .then(ids => {
+          let msg = 'No info published'
+          if (ids.info != null && ids.info.length > 0) {
+            msg = ids.info
+          }
+          return peer + ` -- ${msg}`
+        })
+        .catch(err => `${peer} -- Unable to fetch info: ${err.message}`)
+    )
+  }
+
+  Promise.all(promises)
+    .then(messages => {
+      messages.forEach(m => console.log(m))
+    })
 }
