4abcff7c42d59fd2fa7254897e669ba623f841f3
Arkadiy Kukarkin <arkadiy@mine.nyc>
2016-10-26 17:45:41 -0400

    More params, accept --dirInfo

--- src/peer/repl/commands/repl.js
+++ src/peer/repl/commands/repl.js
@@ -1,14 +1,52 @@
 // @flow
 
 const Node = require('../../node')
-const repl = require('repl')
+const Repl = require('repl')
+const PeerId = require('peer-id')
+const PeerInfo = require('peer-info')
+const Identity = require('../../identity')
+//const config = require('./config')
+
+import type { MediachainNodeOptions } from '../../node'
 
 module.exports = {
   command: 'repl',
   describe: 'start the aleph repl\n',
-  handler: (opts: {directory: string}) => {
-    const {directory} = opts
+  builder: (yargs: Function) => {
+    return yargs
+      .option('dirInfo', {
+        'alias': 'd',
+        'type': 'string',
+        'describe': 'directory to connect to',
+        'demand': false
+      })
+      .help()
+  },
+  handler: (opts: {dirInfo: string}) => {
+    const {dirInfo} = opts
 
-    repl.start('א > ')
+    const options = bootstrap()
+    const node = new Node(options)
+
+    if(dirInfo !== undefined){
+      // FIXME: this needs to actually parse the address
+      node.setDirectory(dirInfo)
+    }
+
+    repl = Repl.start({
+      'prompt': 'א > ',
+      'useColors': true,
+      'ignoreUndefined': true
+    })
+    repl.context.node = node
   }
+}
+
+function bootstrap(): MediachainNodeOptions {
+  const options = {
+    peerId: Identity.generateIdentity(),
+    dirInfo: 'aleph client node'
+  }
+
+  return options
 }
