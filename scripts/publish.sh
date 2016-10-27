f1101c9e4a1e4456bf2af985b7a72f55d9dfd694
Yusef Napora <yusef@napora.org>
2016-10-27 16:59:08 -0400

    add schema hash to publish.sh script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,9 +2,17 @@
 
 NAMESPACE='foo.bar'  # FIXME: should accept this as an argument, need to update ingest-parallel to pass it in
 
+# Hash of schema that must be published on the node before ingestion.
+# You must update this if you change the schema.
+SCHEMA_HASH='QmYGRQYmWC3BAtTAi88mFb7GVeFsUKGM4nm25SBUB9vfc9'
+
+SKIP_VALIDATION='--skipSchemaValidation'
+# if you want to validate every record, use this one instead:
+# SKIP_VALIDATION=''
+
 CONTENT_SELECTOR='--contentSelector _source'
 ID_SELECTOR='--idSelector native_id'
 CONTENT_FILTERS='--contentFilters aesthetics'
 
-mcclient publish ${CONTENT_SELECTOR} ${ID_SELECTOR} ${CONTENT_FILTERS} ${NAMESPACE} $1 > /dev/null
+mcclient publish ${SKIP_VALIDATION} ${CONTENT_SELECTOR} ${ID_SELECTOR} ${CONTENT_FILTERS} ${NAMESPACE} ${SCHEMA_HASH} $1 > /dev/null
 
