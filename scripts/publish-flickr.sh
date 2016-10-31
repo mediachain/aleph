22f79295d63dd3dc9a8b7eb2ad1513016adaa2b5
Yusef Napora <yusef@napora.org>
2016-10-31 16:18:29 -0400

    update publish scripts to use jq

--- scripts/publish-flickr.sh
+++ scripts/publish-flickr.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-NAMESPACE="images.flickr"
-CONTENT_SELECTOR="--contentSelector _source"
-CONTENT_FILTERS="--contentFilters aesthetics"
-ID_SELECTOR="--idSelector native_id"
+NAMESPACE='images.flickr'
 
-mcclient publish ${CONTENT_SELECTOR} ${ID_SELECTOR} ${CONTENT_FILTERS} ${NAMESPACE} $1 > /dev/null
+# Hash of schema that must be published on the node before ingestion.
+# You must update this if you change the schema.
+SCHEMA_HASH='QmYGRQYmWC3BAtTAi88mFb7GVeFsUKGM4nm25SBUB9vfc9'
+
+SKIP_VALIDATION='--skipSchemaValidation'
+# if you want to validate every record, use this one instead:
+# SKIP_VALIDATION=''
+
+mcclient publish ${SKIP_VALIDATION} \
+    --jqFilter '._source | del(.aesthetics)' \
+    --idFilter '.native_id' \
+    ${NAMESPACE} \
+    ${SCHEMA_HASH} \
+    $1 > /dev/null
 
