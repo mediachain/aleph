4ef43d6ce80ce213c4ba956c76a948a26abd34e9
Arkadiy Kukarkin <arkadiy@mine.nyc>
2016-11-03 17:14:37 -0400

    Cobble together REPL

--- src/peer/repl/commands/repl.js
+++ src/peer/repl/commands/repl.js
@@ -1,11 +1,12 @@
 // @flow
 
 const Node = require('../../node')
+// $FlowIssue flow doesn't find repl builtin?
 const Repl = require('repl')
 const PeerId = require('peer-id')
 const PeerInfo = require('peer-info')
 const Identity = require('../../identity')
-//const config = require('./config')
+const Promirepl = require('promirepl')
 
 import type { MediachainNodeOptions } from '../../node'
 
@@ -14,32 +15,63 @@ module.exports = {
   describe: 'start the aleph repl\n',
   builder: (yargs: Function) => {
     return yargs
-      .option('dirInfo', {
+      .option('dir', {
         'alias': 'd',
         'type': 'string',
-        'describe': 'directory to connect to',
+        'describe': 'directory to connect to (multiaddress)',
         'demand': false
       })
       .help()
   },
-  handler: (opts: {dirInfo: string}) => {
-    console.log(opts)
-    const {dirInfo, parentConcat} = opts
+  handler: (opts: {dir: string, remotePeer: string, identityPath: string}) => {
+    const {dir, remotePeer} = opts
 
     const options = bootstrap(opts)
     const node = new Node(options)
 
-    if(dirInfo !== undefined){
-      // FIXME: this needs to actually parse the address
-      node.setDirectory(dirInfo)
+    const commands = {}
+
+    let init, remotePeerInfo
+    if(remotePeer !== undefined){
+      remotePeerInfo = Identity.inflateMultiaddr(remotePeer)
+
+      commands.remoteQuery = function(queryString){
+        return node.remoteQuery(remotePeerInfo, queryString)
+      }
+
+      init = node.start().then(() => {
+        node.openConnection(remotePeerInfo)
+      }).then(() => {
+        console.log("Connected to " + remotePeer)
+      })
+
+    } else {
+      console.log("No remote peer specified, running in detached mode")
+      init = Promise.resolve(undefined)
     }
 
-    repl = Repl.start({
-      'prompt': 'א > ',
-      'useColors': true,
-      'ignoreUndefined': true
+    // TODO: directory stuff
+    if(dir !== undefined){
+      const dirInfo = Identity.inflateMultiaddr(dir)
+      node.setDirectory(dirInfo)
+    } else if(false){
+      // TODO: get directory from remote peer (and amend message below)
+    } else {
+      console.log("No directory specified, running without directory")
+    }
+
+    init.then(() => {
+      const repl = Repl.start({
+        'prompt': 'א > ',
+        'useColors': true,
+        'ignoreUndefined': true
+      })
+      repl.context.node = node
+      repl.context.commands = commands
+      //Promirepl.promirepl(repl)
+    }).catch(err => {
+      console.log(err)
     })
-    repl.context.node = node
   }
 }
 
