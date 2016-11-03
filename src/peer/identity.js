ca8eb32c6a0d4cc5d8cd51efe06264611de9110c
Arkadiy Kukarkin <arkadiy@mine.nyc>
2016-11-03 17:14:37 -0400

    Cobble together REPL

--- src/peer/identity.js
+++ src/peer/identity.js
@@ -1,13 +1,15 @@
 const thenifyAll = require('thenify-all')
 const fs = thenifyAll(require('fs'), {}, ['readFile'])
 const PeerId = thenifyAll(require('peer-id'), {}, ['createFromPrivKey'])
-const crypto = require('libp2p-crypto')
+const PeerInfo = require('peer-info')
+const Crypto = require('libp2p-crypto')
+const Multiaddr = require('multiaddr')
 
 const KEY_TYPE = 'RSA'  // change to ECC when possible
 const KEY_BITS = 1024
 
 function generateIdentity (): PeerId {
-  const key = crypto.generateKeyPair(KEY_TYPE, KEY_BITS)
+  const key = Crypto.generateKeyPair(KEY_TYPE, KEY_BITS)
   return new PeerId(key.public.hash(), key)
 }
 
@@ -16,7 +18,7 @@ function saveIdentity (peerId: PeerId, filePath: string) {
     throw new Error('PeerID has no private key, cannot persist')
   }
 
-  const privKeyBytes = crypto.marshalPrivateKey(peerId.privKey, KEY_TYPE)
+  const privKeyBytes = Crypto.marshalPrivateKey(peerId.privKey, KEY_TYPE)
   fs.writeFileSync(filePath, privKeyBytes)
 }
 
@@ -39,9 +41,18 @@ function loadOrGenerateIdentity(filePath: string): PeerId {
   return peerId
 }
 
+function inflateMultiaddr(multiaddrString: string): PeerInfo {
+    const multiaddr = Multiaddr(multiaddrString)
+    const peerInfo = new PeerInfo()
+    peerInfo.multiaddr.add(multiaddr)
+
+    return peerInfo
+}
+
 module.exports = {
   generateIdentity,
   saveIdentity,
   loadIdentity,
-  loadOrGenerateIdentity
+  loadOrGenerateIdentity,
+  inflateMultiaddr
 }
