1dcdf0be6e45e73c4aaf49d233c4e460c10af962
Yusef Napora <yusef@napora.org>
2016-11-10 15:59:57 -0500

    increase input size limit for cbor encoder

--- src/metadata/serialize.js
+++ src/metadata/serialize.js
@@ -1,10 +1,30 @@
 // @flow
 
-// For now, this is just a very thin wrapper around the cbor lib.
-// It exists so that we can swap out the implementation later without changing call sites
-
 const cbor = require('cbor')
-const {encode, decode} = cbor
+const NoFilter = require('nofilter')
+const {Encoder, decode} = cbor
+
+// the default encoder fails if given input > 16KB.  Up this to 1MB to support big objects
+const MAX_INPUT_SIZE = 1024 * 1024
+
+// The default cbor Encoder.encode function, with the the stream highWaterMark overridden
+function encode () {
+  const objs = Array.prototype.slice.apply(arguments)
+  const enc = new Encoder({highWaterMark: MAX_INPUT_SIZE})
+  const bs = new NoFilter()
+  enc.pipe(bs)
+  for (const o of objs) {
+    if (typeof o === 'undefined') {
+      enc._pushUndefined()
+    } else if (o === null) {
+      enc._pushObject(null)
+    } else {
+      enc.write(o)
+    }
+  }
+  enc.end()
+  return bs.read()
+}
 
 module.exports = {
   encode,
