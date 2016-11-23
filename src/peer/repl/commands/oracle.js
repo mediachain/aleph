f340341b1cc69c0148d4ea16140a8ad28731dc5a
Arkadiy Kukarkin <arkadiy@mindhole.org>
2016-11-23 16:22:34 -0500

    Add rudimentary web3 connection

---
+++ src/peer/repl/commands/oracle.js
@@ -0,0 +1,71 @@
+// @flow
+
+const os = require('os')
+const { bootstrap } = require('../util')
+const web3 = require('web3');
+
+module.exports = {
+  command: 'oracle',
+  describe: 'start an ethereum oracle\n',
+  builder: (yargs: Function) => {
+    return yargs
+      .option('dir', {
+        'alias': 'd',
+        'type': 'string',
+        'describe': 'directory to connect to (multiaddress)',
+        'demand': false
+      })
+      .help()
+  },
+  handler: (opts: {dir?: string, remotePeer?: string, identityPath: string}) => {
+    const {remotePeer} = opts
+
+    bootstrap(opts)
+      .catch(err => {
+        console.error(`Error setting up aleph node: ${err.message}`)
+        process.exit(1)
+      })
+      .then(({node, remote}) => {
+        let init
+        if (remote != null) {
+          init = node.start()
+            .then(() => node.openConnection(remote.remotePeerInfo))
+            .then(() => { console.log(`Connected to `, remotePeer) })
+        } else {
+          console.log('No remote peer specified, running in detached mode')
+          // TODO: actually we want to die here, given that we don't have discovery
+          init = node.start()
+        }
+
+        // TODO: directory stuff
+        if (node.directory == null) {
+          // TODO: get directory from remote peer (and amend message below)
+          console.log('No directory specified, running without directory')
+        }
+
+        init.then(() => {
+          web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+          console.log(web3);
+        }).catch(err => {
+          console.log(err)
+        })
+      })
+  }
+}
+
+const EMPTY = '(' + os.EOL + ')'
+const promiseEval = (defaultEval) => (cmd, context, filename, callback) => {
+  if (cmd === EMPTY) return callback()
+  defaultEval(cmd, context, filename, (err, result) => {
+    if (err) { return callback(err) }
+
+    if (result instanceof Promise) {
+      result.then(
+        asyncResult => { callback(null, asyncResult) },
+        asyncErr => { callback(asyncErr) }
+      )
+    } else {
+      callback(null, result)
+    }
+  })
+}
