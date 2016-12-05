0448421023586846b9012441878ca91de31a5047
Yusef Napora <yusef@napora.org>
2016-12-05 11:52:41 -0500

    try to download jq binary, fall back to compiling

--- scripts/build-jq.js
+++ scripts/build-jq.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
+const os = require('os')
+const fs = require('fs')
+const path = require('path')
 const BinBuild = require('bin-build')
 const mkdirp = require('mkdirp')
+const { get } = require('lodash')
+const fetch = require('node-fetch')
 
 const JQ_INFO = {
   name: 'jq',
@@ -9,11 +14,23 @@ const JQ_INFO = {
   version: 'jq-1.5'
 }
 
-const path = require('path')
+const BINARY_NAMES = {
+  linux: {
+    x86: 'jq-linux32',
+    x64: 'jq-linux64'
+  },
+  darwin: {
+    x64: 'jq-osx-amd64'
+  },
+  win32: {
+    x86: 'jq-win32.exe',
+    x64: 'jq-win64.exe'
+  }
+}
+
 const outputDir = path.join(__dirname, '..', 'bin')
 const outputPath = path.join(outputDir, 'jq')
 
-const fs = require('fs')
 try {
   fs.accessSync(outputPath, fs.F_OK)
   // already exists
@@ -26,13 +43,45 @@ const build = new BinBuild()
   .cmd('make')
   .cmd(`cp ./jq ${outputPath}`)
 
-console.log('building jq...')
-mkdirp.sync(outputDir)
-build.run((err) => {
-  if (err) {
-    console.log('err', err)
-    process.exit(1)
-  } else {
-    console.log(`jq compiled to ${outputPath}`)
+function downloadBinary (destinationPath) {
+  const platform = os.platform()
+  const arch = os.arch()
+  const binName = get(BINARY_NAMES, [platform, arch])
+  if (binName == null) {
+    throw new Error(`No jq binary for ${platform}/${arch}`)
   }
-})
+
+  const binUrl = [JQ_INFO.url, JQ_INFO.version, binName].join('/')
+  console.log('Downloading jq binary from ', binUrl)
+  return fetch(binUrl)
+    .then(response => new Promise((resolve, reject) => {
+      const output = fs.createWriteStream(destinationPath)
+      response.body.on('error', reject)
+      output.on('error', reject)
+      output.on('close', resolve)
+      response.body.pipe(output)
+    }))
+    .then(() => {
+      if (platform !== 'win32') {
+        fs.chmodSync(destinationPath, '755')
+      }
+    })
+}
+
+mkdirp.sync(outputDir)
+
+downloadBinary(outputPath)
+  .catch(err => {
+    console.log('Error downloading jq binary: ', err.message)
+    console.log('building jq...')
+
+    build.run((err) => {
+      if (err) {
+        console.log('err', err)
+        process.exit(1)
+      } else {
+        console.log(`jq compiled to ${outputPath}`)
+      }
+    })
+  })
+
