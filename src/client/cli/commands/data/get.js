be2932a7215cd091c5426fc8c2f7edbf21a1e7d0
Yusef Napora <yusef@napora.org>
2016-12-09 16:27:21 -0500

    only wrap with object in pretty mode, add --batch flag

--- src/client/cli/commands/data/get.js
+++ src/client/cli/commands/data/get.js
@@ -7,7 +7,8 @@ import type { Readable as ReadableStream } from 'stream'
 module.exports = {
   command: 'get <objectIds..>',
   description: 'Request one or more `objectIds` from the local node and print to the console.' +
-    'If multiple ids are given, results will be returned as a JSON map, with objectIds as keys.\n',
+    'If multiple ids are given and pretty-printing is enabled, results will be returned as a JSON map, ' +
+    'with `objectIds` as keys.\n',
   builder: {
     color: {
       type: 'boolean',
@@ -20,55 +21,81 @@ module.exports = {
       description: 'Pretty print the output.\n',
       default: true,
       defaultDescription: 'True.  Use --no-pretty for compact output.'
+    },
+    batch: {
+      type: 'boolean',
+      description: 'Force "batch-mode", even if only one key is given.  Disables color and pretty-printing.\n',
+      default: false
     }
   },
 
-  handler: subcommand((opts: {client: RestClient, objectIds: Array<string>, color: ?boolean, pretty: boolean}) => {
-    const {client, objectIds, color, pretty} = opts
+  handler: subcommand((opts: {client: RestClient, objectIds: Array<string>, color: ?boolean, pretty: boolean, batch: boolean}) => {
+    const {client, objectIds, batch} = opts
+    let {color, pretty} = opts
+    if (batch) {
+      color = false
+      pretty = false
+    }
 
-    if (objectIds.length === 1) {
+    if (!batch && objectIds.length === 1) {
       return client.getData(objectIds[0])
         .then(objectFormatter(color, pretty))
         .then(output => process.stdout.write(output))
     }
 
     return client.batchGetDataStream(objectIds)
-      .then(stream => printStream(stream, objectIds, color, pretty))
+      .then(stream => {
+        if (pretty) {
+          return printStreamPretty(stream, objectIds, color)
+        } else {
+          return printStreamCompact(stream)
+        }
+      })
   })
 }
 
-function printStream (stream: ReadableStream, objectIds: Array<string>, color: ?boolean, pretty: boolean): Promise<void> {
-  const formatObject = objectFormatter(color, pretty)
-  const {stdout} = process
-  stdout.write('{')
-  if (pretty) {
-    stdout.write('\n')
-  }
-
+function printStreamCompact (stream: ReadableStream): Promise<void> {
   return new Promise((resolve, reject) => {
-    const leftPad = pretty ? '  ' : ''
+    const formatObject = objectFormatter(false, false)
+    const {stdout} = process
+    stream.on('data', obj => {
+      stdout.write(formatObject(obj))
+    })
+    stream.on('end', resolve)
+    stream.on('error', reject)
+  })
+}
+
+function printStreamPretty (stream: ReadableStream, objectIds: Array<string>, color: ?boolean, wrap: boolean = true): Promise<void> {
+  const formatObject = objectFormatter(color, true)
+  const {stdout} = process
+  const padding = wrap ? 2 : 0
+  if (wrap) {
+    stdout.write('{\n')
+  }
+  return new Promise((resolve, reject) => {
     stream.on('data', result => {
       const key = objectIds.pop()
       const keyString = formatObject(key).trim()
       const formatted = formatObject(result).trim()
-        .split('\n')
-        .map((s, i) => (i === 0) ? s : leftPad + s)
-        .join('\n')
+      const output = indent(`${keyString}: ${formatted}`, padding)
+      stdout.write(output)
 
-      stdout.write(`${leftPad}${keyString}: ${formatted}`)
-      if (objectIds.length > 0) {
+      if (wrap && objectIds.length > 0) {
         stdout.write(',')
       }
-      if (pretty) {
-        stdout.write('\n')
-      }
+      stdout.write('\n')
     })
     stream.on('error', (err) => {
-      stdout.write(`}\n`)
+      if (wrap) {
+        stdout.write(`}\n`)
+      }
       reject(err)
     })
     stream.on('end', () => {
-      stdout.write(`}\n`)
+      if (wrap) {
+        stdout.write(`}\n`)
+      }
       resolve()
     })
   })
@@ -80,4 +107,9 @@ const objectFormatter = (color: ?boolean, pretty: boolean) => (obj: ?mixed): str
   }
 
   return formatJSON(obj, {color, pretty})
+}
+
+function indent (s: string, spaces: number = 2): string {
+  const padding = new Array(spaces).fill(' ').join('')
+  return s.split('\n').map(line => padding + line).join('\n')
 }
