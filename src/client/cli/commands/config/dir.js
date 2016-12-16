886c6f7a90512acefb3e24b684c9a7ec5647e156
Yusef Napora <yusef@napora.org>
2016-12-16 12:05:32 -0500

    add --clear flag to clear directory config

--- src/client/cli/commands/config/dir.js
+++ src/client/cli/commands/config/dir.js
@@ -6,17 +6,29 @@ const { subcommand, println, pluralizeCount } = require('../../util')
 module.exports = {
   command: 'dir [dirIds..]',
   description: 'Get or set the directory servers.\n',
-  handler: subcommand((opts: {client: RestClient, dirIds: Array<string>}) => {
-    const {client, dirIds} = opts
+  builder: {
+    clear: {
+      type: 'boolean',
+      description: 'If given, clear out the existing directory configuration.\n'
+    }
+  },
+  handler: subcommand((opts: {client: RestClient, dirIds: Array<string>, clear?: boolean}) => {
+    const {client, dirIds, clear} = opts
+
+    if (clear) {
+      return client.setDirectoryIds()
+        .then(() => println('Directory configuration cleared'))
+    }
+
     if (dirIds.length > 0) {
       return client.setDirectoryIds(...dirIds)
         .then(() => {
           println(`Set ${pluralizeCount(dirIds.length, 'directory server')}:`)
           dirIds.forEach(println)
         })
-    } else {
-      return client.getDirectoryIds()
-        .then(ids => ids.forEach(println))
     }
+
+    return client.getDirectoryIds()
+      .then(ids => ids.forEach(println))
   })
 }
