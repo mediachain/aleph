b9278abf5b55decce101ecbd32d373c200cd4854
Yusef Napora <yusef@napora.org>
2017-01-09 10:49:33 -0500

    make net subcommand directory, alias netAddr and netConnections

---
+++ src/client/cli/commands/net/ping.js
@@ -0,0 +1,20 @@
+// @flow
+
+const RestClient = require('../../../api/RestClient')
+const { subcommand, println } = require('../../util')
+
+module.exports = {
+  command: 'ping <peerId>',
+  describe: 'Ping a remote peer, identified by `peerId`, using the libp2p ping protocol. ' +
+  'Will attempt to lookup the peer with a configured directory server or DHT.\n',
+  handler: subcommand((opts: {peerId: string, client: RestClient}) => {
+    const {peerId, client} = opts
+    println('Pinging peer: ', peerId)
+
+    return client.ping(peerId)
+      .then(
+        success => println('Ping OK'),
+        err => { throw new Error(`Error pinging: ${err.message}`) }
+      )
+  })
+}
