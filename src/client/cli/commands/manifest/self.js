7c10c3f80f4a5e4405f4690965839b2004008c9a
Yusef Napora <yusef@napora.org>
2017-01-13 09:57:37 -0500

    rename `manifest node` cmd to `manifest self`

--- src/client/cli/commands/manifest/self.js
+++ src/client/cli/commands/manifest/self.js
@@ -4,8 +4,9 @@ const RestClient = require('../../../api/RestClient')
 const { subcommand, println } = require('../../util')
 
 module.exports = {
-  command: 'node',
-  description: `Get the "node manifest" for the local node, suitable for signing by mcid to produce a manifest.\n`,
+  command: 'self',
+  description: `Get the unsigned "node manifest" for the local node, ` +
+    `suitable for signing by mcid to produce a manifest.\n`,
   handler: subcommand((opts: {client: RestClient}) => {
     const {client} = opts
     return client.getNodeManifest()
