74d7977034c37c30ebd06a20807c0569d45e5d0d
Yusef Napora <yusef@napora.org>
2017-02-08 17:37:16 -0500

    doc comments for P2PNode class

--- src/peer/libp2p_node.js
+++ src/peer/libp2p_node.js
@@ -17,6 +17,22 @@ import type { Connection } from 'interface-connection'
 const OFFLINE_ERROR_MESSAGE = 'The libp2p node is not started yet'
 const DEFAULT_DIAL_TIMEOUT = 10000
 
+/**
+ * Options for {@link P2PNode} constructor.
+ * @property peerInfo
+ *  A libp2p PeerInfo object that identifies a peer.
+ *  Must contain at least one valid multiaddr for the node to listen on.
+ *
+ * @property peerBook
+ *  An optional PeerBook object to "pre-seed" the node with mappings from PeerId to PeerInfo.
+ *
+ * @property disableSecureIO
+ *  If true, disables encryption for streams opened by the node. Use for testing only.
+ *
+ * @property timeout
+ *  Timeout in milliseconds to wait when dialing a peer before erroring out.
+ *  Default is 10000 (10s) if this option is not present.
+ */
 type P2PNodeOptions = {
   peerInfo: PeerInfo,
   peerBook?: PeerBook,
@@ -24,6 +40,23 @@ type P2PNodeOptions = {
   dialTimeout?: number
 }
 
+/**
+ * An object that acts as a LibP2P peer.  Provides promise-based interfaces
+ * for [opening]{@link P2PNode#dialByPeerInfo} and [closing]{@link P2PNode#hangUpByPeerInfo}
+ * streams to other peers, as well as [handling named protocols]{@link P2PNode#handle}.
+ *
+ * A newly-constructed `P2PNode` handles a single protocol, the libp2p standard Ping protocol.
+ * You can ping another libp2p node (including IPFS nodes) with {@link P2PNode#ping}.
+ *
+ * `P2PNode`s begin offline, and you must call {@link P2PNode#start} to take them online.
+ *
+ * A `PeerInfo` object with at least one multiaddr is required when constructing a `P2PNode`.
+ * The node will attempt to listen on all tcp and / or websocket multiaddrs contained in the `PeerInfo`.
+ * The node must be listening on a transport in order to dial with it; if no websocket listen
+ * addresses are provided, you won't be able to dial to a peer using websockets.  Note that concat
+ * nodes only support tcp transports, so that's probably what you want to use.
+ *
+ */
 class P2PNode {
   peerInfo: PeerInfo
   peerBook: PeerBook
@@ -32,6 +65,10 @@ class P2PNode {
   abortables: Set<Abortable>
   dialTimeout: number
 
+  /**
+   * Create a P2PNode.
+   * @param {P2PNodeOptions} options
+   */
   constructor (options: P2PNodeOptions) {
     let {peerInfo, peerBook, disableSecureIO, dialTimeout} = options
     this.isOnline = false
@@ -64,12 +101,38 @@ class P2PNode {
     this.abortables = new Set()
   }
 
+  /**
+   * Creates and returns a pull-stream "abortable", which can be
+   * added to a pull-stream pipeline to allow aborting the stream
+   * at any time. All abortables returned from this method will be
+   * aborted when the node is [stopped]{@link P2PNode#stop}
+   *
+   * @example
+   * const abortable = myNode.newAbortable()
+   * pull(
+   *   connectionToOtherNode,
+   *   abortable,
+   *   handleLongRunningProcess(),
+   * )
+   *
+   * // later...
+   * abortable.abort() // -> closes the stream
+   *
+   * @returns {Object} a pull-stream abortable.  Call `.abort()` to cancel
+   * a stream that passes through the abortable.
+   */
   newAbortable (): Abortable {
     const abortable = Abortable()
     this.abortables.add(abortable)
     return abortable
   }
 
+  /**
+   * Start the node, bringing it online.  This causes the node to listen on
+   * all available transports, and allows the node to dial out to other peers.
+   * @returns {Promise<void>} resolves with no value on success, or rejects with an
+   * `Error` if the node can't be started.
+   */
   start (): Promise<void> {
     if (this.isOnline) return Promise.resolve()
 
@@ -96,6 +159,16 @@ class P2PNode {
     })
   }
 
+  /**
+   * Stop the node, taking it offline.
+   * This will cancel any "abortables" returned from {@link P2PNode#newAbortable},
+   * and close any open transports.
+   *
+   * Note that this may take several seconds using the TCP transport, even if there
+   * are no streams open.
+   * @returns {Promise<void>} resolves with no value on success, or rejects with an `Error`
+   * if the node can't be stopped.
+   */
   stop (): Promise<void> {
     if (!this.isOnline) return Promise.resolve()
 
@@ -112,6 +185,15 @@ class P2PNode {
     })
   }
 
+  /**
+   * Set whether to use encrypted streams for communication with other nodes.
+   * The default is true, and should usually be left alone. Disabling secure IO
+   * is useful during unit testing, where security isn't a concern and the overhead
+   * of encryption can slow down test execution.
+   *
+   * Note that a node with secure IO disabled cannot communicate with a node that
+   * requires secure IO.
+   */
   setSecureIOEnabled (use: boolean = true) {
     if (use) {
       this.swarm.connection.crypto(secio.tag, secio.encrypt)
@@ -120,6 +202,12 @@ class P2PNode {
     }
   }
 
+  /**
+   * Connect to a `peer`, opening a stream named by the `protocol` string.
+   * @param peer - a `PeerInfo` object with at least one valid multiaddr for the node you want to dial.
+   * @param protocol - a string identifying a P2P protocol, e.g. `"/mediachain/node/query"`
+   * @returns {Promise<Connection>} resolves to a libp2p Connection, which can be used in a pull-stream.
+   */
   dialByPeerInfo (peer: PeerInfo, protocol: string): Promise<Connection> {
     if (!this.isOnline) {
       return Promise.reject(new Error(OFFLINE_ERROR_MESSAGE))
@@ -138,6 +226,12 @@ class P2PNode {
     return promiseTimeout(this.dialTimeout, dialPromise)
   }
 
+  /**
+   * Close all open streams to the given peer.
+   * @param peer a `PeerInfo` object identifying a peer with open streams.
+   * @returns {Promise} resolves with no value on success, or rejects with an
+   * `Error` if one occurs during the hang up operation.
+   */
   hangUpByPeerInfo (peer: PeerInfo): Promise<void> {
     if (!this.isOnline) {
       return Promise.reject(new Error(OFFLINE_ERROR_MESSAGE))
@@ -149,14 +243,30 @@ class P2PNode {
     })
   }
 
-  handle (protocol: string, handler: Function): any {
+  /**
+   * Register a `handler` for the named `protocol`.
+   * @param protocol - a string identifying a libp2p protocol, e.g. `"/mediachain/node/query"`
+   * @param handler - a function that will be called with the name of the protocol and a libp2p Connection
+   * for incoming streams.
+   */
+  handle (protocol: string, handler: (protocol: string, connection: Connection) => void): any {
     return this.swarm.handle(protocol, handler)
   }
 
+  /**
+   * Remove any handlers registered for the named `protocol`
+   * @param protocol - a string identifying a previously handled protocol.
+   */
   unhandle (protocol: string): any {
     return this.swarm.unhandle(protocol)
   }
 
+  /**
+   * Send a libp2p Ping message to the given `peer` and wait for a response.
+   * @param peer - a `PeerInfo` for a peer, with at least one valid multiaddr.
+   * @returns {Promise<number>} - resolves to the latency (in msec), or rejects
+   * with an `Error` if the ping fails.
+   */
   ping (peer: PeerInfo): Promise<number> {
     const pingPromise = new Promise((resolve, reject) => {
       const p = new Ping(this.swarm, peer)
